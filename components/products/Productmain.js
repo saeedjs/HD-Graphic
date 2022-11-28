db988f6b4ba572b666197195d96b1ea183ea5f7b
Ahmadreza Nouri   FrontEnd-ReactJs <104431754+ahmadrezanouri1@users.noreply.github.com>
2022-11-28 10:09:00 -0800

    change again in product main

--- components/products/Productmain.js
+++ components/products/Productmain.js
@@ -18,13 +18,13 @@ const ProductMain = () => {
       </Grid>
 
       <Grid container justifyContent="center">
-        <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={4}>
+        <Grid item sx={{ margin: "10px" }} xs={12} md={12} lg={4}>
           <Paper>
-            <Card sx={{ maxWidth: 500 }}>
+            <Card sx={{ minWidth: 400, maxWidth: 500 }}>
               <CardActionArea>
                 <CardMedia
                   component="img"
-                  height="400"
+                  height="411"
                   image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
                   alt="green iguana"
                 />
@@ -32,7 +32,7 @@ const ProductMain = () => {
             </Card>
           </Paper>
         </Grid>
-        <Grid item lg={2.5}>
+        <Grid item xs={12} lg={2.5}>
           <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
             <Paper>
               <Card sx={{ maxWidth: 500 }}>
@@ -62,7 +62,7 @@ const ProductMain = () => {
             </Paper>
           </Grid>
         </Grid>
-        <Grid item lg={2.5}>
+        <Grid item xs={12} lg={2.5}>
           <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
             <Paper>
               <Card sx={{ maxWidth: 500 }}>
@@ -92,7 +92,7 @@ const ProductMain = () => {
             </Paper>
           </Grid>
         </Grid>
-        <Grid item lg={2.5}>
+        <Grid item xs={12} lg={2.5}>
           <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
             <Paper>
               <Card sx={{ maxWidth: 500 }}>
