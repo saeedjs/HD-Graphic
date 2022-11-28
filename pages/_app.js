d7175bd58346aedc75c06d8b7d28a9bb987d29d8
saeedjs <saied.2fm83@gmail.com>
2022-11-28 18:46:11 +0330

    saeed commit layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import Header from '../components/layout/Header'
+import Footer from '../components/layout/Footer'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Header />
+    <Component {...pageProps} />
+    <Footer />
+  </> )
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <>
-      <Main></Main>
+      main
     </>
   );
 }
