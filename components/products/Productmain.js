8990e473ec955d84a8247d07bd06fdde9c5f9dc9
Ahmadreza Nouri   FrontEnd <ahmadrezanouri.ir@gmail.com>
2022-11-28 07:17:58 -0800

    change  products main

--- components/products/Productmain.js
+++ components/products/Productmain.js
@@ -1,5 +1,24 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
 const ProductMain = () => {
-  return <></>;
+  return (
+    <>
+      <Card sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="240"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            alt="green iguana"
+          />
+        </CardActionArea>
+      </Card>
+    </>
+  );
 };
 
 export default ProductMain;
