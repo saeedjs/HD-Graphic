d439c3af252597da7043cb482bdf0f801146ac02
saeedjs <saied.2fm83@gmail.com>
2022-11-28 19:04:07 +0330

    saeed commit

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,11 +1,13 @@
+import * as React from 'react';
 
+import Box from '@mui/material/Box';
 
 const Footer = () => {
   return (
     <>
       <Box
         sx={{
-          width: 300,
+          width: '100%',
           height: 300,
           backgroundColor: "primary.dark",
           "&:hover": {
