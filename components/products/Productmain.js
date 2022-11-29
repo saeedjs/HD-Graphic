906f017aeefbade5bb20bee9f0c7a127f0635187
Ahmadreza Nouri   FrontEnd <ahmadrezanouri.ir@gmail.com>
2022-11-28 20:55:07 -0800

    commit add

--- components/products/Productmain.js
+++ components/products/Productmain.js
@@ -2,19 +2,41 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { CardActionArea } from "@mui/material";
+import { CardActionArea, Grid } from "@mui/material";
 
 const ProductMain = () => {
   return (
     <>
       <Card sx={{ maxWidth: 345 }}>
         <CardActionArea>
-          <CardMedia
-            component="img"
-            height="240"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            alt="green iguana"
-          />
+          <Grid container spacing={2}>
+            <Grid item xs={8}>
+              <CardMedia
+                component="img"
+                height="240"
+                image="https://picsum.photos/200"
+                alt="green iguana"
+              />
+            </Grid>
+            <CardMedia
+              component="img"
+              height="240"
+              image="https://picsum.photos/200"
+              alt="green iguana"
+            />
+            <CardMedia
+              component="img"
+              height="240"
+              image="https://picsum.photos/200"
+              alt="green iguana"
+            />
+            <CardMedia
+              component="img"
+              height="240"
+              image="https://picsum.photos/200"
+              alt="green iguana"
+            />
+          </Grid>
         </CardActionArea>
       </Card>
     </>
