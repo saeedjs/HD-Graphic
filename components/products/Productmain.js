eb35500d3c8e931a9513d5b64559928c987ca446
Ahmadreza Nouri   FrontEnd <ahmadrezanouri.ir@gmail.com>
2022-11-28 21:38:34 -0800

    Revert "change again in product main"

--- components/products/Productmain.js
+++ components/products/Productmain.js
@@ -6,40 +6,123 @@ import { CardActionArea, Grid } from "@mui/material";
 
 const ProductMain = () => {
   return (
-    <>
-      <Card sx={{ maxWidth: 345 }}>
-        <CardActionArea>
-          <Grid container spacing={2}>
-            <Grid item xs={8}>
-              <CardMedia
-                component="img"
-                height="240"
-                image="https://picsum.photos/200"
-                alt="green iguana"
-              />
-            </Grid>
-            <CardMedia
-              component="img"
-              height="240"
-              image="https://picsum.photos/200"
-              alt="green iguana"
-            />
-            <CardMedia
-              component="img"
-              height="240"
-              image="https://picsum.photos/200"
-              alt="green iguana"
-            />
-            <CardMedia
-              component="img"
-              height="240"
-              image="https://picsum.photos/200"
-              alt="green iguana"
-            />
+    <Container>
+      <Grid container>
+        <Grid item xs={6}>
+          <Typography>متن نمایشی برای </Typography>
+        </Grid>
+        <Grid item xs={6}>
+          <Typography>متن نمایشی برای </Typography>
+        </Grid>
+      </Grid>
+
+      <Grid container justifyContent="center">
+        <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={4}>
+          <Paper>
+            <Card sx={{ maxWidth: 500 }}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="400"
+                  image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
+                  alt="green iguana"
+                />
+              </CardActionArea>
+            </Card>
+          </Paper>
+        </Grid>
+        <Grid item lg={2.5}>
+          <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
+            <Paper>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
+                    alt="green iguana"
+                  />
+                </CardActionArea>
+              </Card>
+            </Paper>
           </Grid>
-        </CardActionArea>
-      </Card>
-    </>
+          <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
+            <Paper>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
+                    alt="green iguana"
+                  />
+                </CardActionArea>
+              </Card>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item lg={2.5}>
+          <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
+            <Paper>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
+                    alt="green iguana"
+                  />
+                </CardActionArea>
+              </Card>
+            </Paper>
+          </Grid>
+          <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
+            <Paper>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
+                    alt="green iguana"
+                  />
+                </CardActionArea>
+              </Card>
+            </Paper>
+          </Grid>
+        </Grid>
+        <Grid item lg={2.5}>
+          <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
+            <Paper>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
+                    alt="green iguana"
+                  />
+                </CardActionArea>
+              </Card>
+            </Paper>
+          </Grid>
+          <Grid item sx={{ margin: "10px" }} xs={12} md={6} lg={12}>
+            <Paper>
+              <Card sx={{ maxWidth: 500 }}>
+                <CardActionArea>
+                  <CardMedia
+                    component="img"
+                    height="200"
+                    image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1nFe39zYThqDWTPcThwDm0H9axjBpeI3DuQ&usqp=CAU"
+                    alt="green iguana"
+                  />
+                </CardActionArea>
+              </Card>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
