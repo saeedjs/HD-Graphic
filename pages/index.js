93eae3f609694e791b4619a00ebb0a110b89ace0
Ahmadreza Nouri   FrontEnd <ahmadrezanouri.ir@gmail.com>
2022-11-29 00:28:57 -0800

    change product main

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import ProductMain from "../components/products/Productmain";
+import Productmain2 from "../components/products/Productmain2";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <>
       <ProductMain />
+      <Productmain2 />
     </>
   );
 }
