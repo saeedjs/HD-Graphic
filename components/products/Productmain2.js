87386df3d45a594515a38b1c44930f733bbf3111
Ahmadreza Nouri   FrontEnd <ahmadrezanouri.ir@gmail.com>
2022-11-29 03:00:39 -0800

    almost complate products page

--- components/products/Productmain2.js
+++ components/products/Productmain2.js
@@ -3,67 +3,24 @@ import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import { Container, Paper, CardActionArea, Grid } from "@mui/material";
+import Image from "next/image";
 
 const Productmain2 = () => {
   return (
     <>
       <Container>
-        <Grid container sx={{ maxWidth: "50%" }}>
-          <Grid item>
-            <Paper>
-              <Card sx={{ maxWidth: 280 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="310"
-                    image="/images/99996.png"
-                    alt="green iguana"
-                  />
-                </CardActionArea>
-              </Card>
-            </Paper>
+        <Grid container sx={{ maxWidth: "100%" }}>
+          <Grid item lg={6}>
+            <Image width={280} height={310} src={"/images/1234.png"} />
           </Grid>
-          <Grid item>
-            <Paper>
-              <Card sx={{ maxWidth: 280 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="310"
-                    image="/images/99996.png"
-                    alt="green iguana"
-                  />
-                </CardActionArea>
-              </Card>
-            </Paper>
+          <Grid item lg={6}>
+            <Image width={280} height={317} src={"/images/123.png"} />
           </Grid>
-          <Grid item>
-            <Paper>
-              <Card sx={{ maxWidth: 280 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="310"
-                    image="/images/123.png"
-                    alt="green iguana"
-                  />
-                </CardActionArea>
-              </Card>
-            </Paper>
+          <Grid item lg={6}>
+            <Image width={280} height={317} src={"/images/12345.png"} />
           </Grid>
-          <Grid item>
-            <Paper>
-              <Card sx={{ maxWidth: 280 }}>
-                <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="310"
-                    image="/images/1234.png"
-                    alt="green iguana"
-                  />
-                </CardActionArea>
-              </Card>
-            </Paper>
+          <Grid item lg={6}>
+            <Image width={280} height={317} src={"/images/99996.png"} />
           </Grid>
         </Grid>
       </Container>
