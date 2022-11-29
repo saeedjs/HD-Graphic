aeceb0c848daa1afe46c3b15b81180c3a2853afa
abolfazle79 <abolfazle.abolghasemi79@gmail.com>
2022-11-29 17:01:34 +0330

    v2 navabrTop

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import ProductMain from "../components/products/Productmain";
 import Productmain2 from "../components/products/Productmain2";
 import styles from "../styles/Home.module.css";
-
 export default function Home() {
   return (
     <>
