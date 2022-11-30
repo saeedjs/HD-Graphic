dafbc6e6cf6158efcf8f82d9f9e0c0fed0ca68fc
abolfazle79 <abolfazle.abolghasemi79@gmail.com>
2022-11-30 14:01:42 +0330

    landing v1

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,31 @@ import Productmain2 from "../components/products/Productmain2";
 import styles from "../styles/Home.module.css";
 import Productmain3 from "../components/products/Productmain3";
 import TitleProducts from "../components/products/title";
+import Landing from "../components/Header/Landing";
 
 export default function Home() {
   return (
     <Container Container>
-      <Grid container>
+      <Landing />
+      {/* <Grid container>
         <Grid item lg={4}>
           <TitleProducts />
         </Grid>
         <Grid item lg={8}>
           <TitleProducts />
         </Grid>
-      </Grid>
+      </Grid> */}
 
-      <ProductMain />
-      <Grid container>
+      {/* <ProductMain /> */}
+      {/* <Grid container>
         <Grid item lg={6}>
           <TitleProducts />
         </Grid>
         <Grid item lg={6}>
           <TitleProducts />
         </Grid>
-      </Grid>
-      <Productmain3 />
+      </Grid> */}
+      {/* <Productmain3 /> */}
     </Container>
   );
 }
