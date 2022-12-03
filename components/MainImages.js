2f1f3d5c0aa26f5d299f68727cda783b8d44af9c
saeedjs <saied.2fm83@gmail.com>
2022-12-03 17:01:47 +0330

    saeed

--- components/MainImages.js
+++ components/MainImages.js
@@ -19,18 +19,52 @@ const Mainimages = () => {
           }}
         >
           <Grid container>
-            <Grid item xs={4.3}>
-              <Box sx={{ display: "flex", flexDirection: "column", height:'100%', alignItems:'center', justifyContent:'center' }}>
+            <Grid item xs={6} md={4.3}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
                 <Box>
-                  <Typography sx={{fontWeight:'bold', fontSize:'24px', mb:'13px'}}>عضویت کاربر عادی</Typography>
+                  <Typography
+                    sx={{ fontWeight: "bold", fontSize: "24px", mb: "13px" }}
+                  >
+                    عضویت کاربر عادی
+                  </Typography>
                 </Box>
                 <Box>
-                  <Button variant="contained" width='119px' height='32px' sx={{backgroundColor:'colors.green', borderRadius:'10px', boxShadow:'none'}}>عضویت کاربر عادی</Button>
+                  <Button
+                    variant="contained"
+                    width="119px"
+                    height="32px"
+                    sx={{
+                      backgroundColor: "colors.green",
+                      borderRadius: "10px",
+                      boxShadow: "none",
+                      "&:hover": {
+                        backgroundColor: "colors.green",
+                      },
+                    }}
+                  >
+                    عضویت کاربر عادی
+                  </Button>
                 </Box>
               </Box>
             </Grid>
-            <Grid item xs={4}>
-              <Box sx={{ mt: "-20px" }}>
+            <Grid item md={4}>
+              <Box
+                sx={{
+                  mt: "-20px",
+                  display: {
+                    md: "block",
+                    xs: "none",
+                  },
+                }}
+              >
                 <img
                   height={"214px"}
                   sx={{ width: "286.5px" }}
@@ -38,13 +72,39 @@ const Mainimages = () => {
                 />
               </Box>
             </Grid>
-            <Grid item xs={3.7}>
-              <Box sx={{ display: "flex", flexDirection: "column", height:'100%', alignItems:'center', justifyContent:'center' }}>
+            <Grid item xs={6} md={3.7}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
                 <Box>
-                  <Typography sx={{fontWeight:'bold', fontSize:'24px', mb:'13px'}}>عضویت طراح یا عکاس</Typography>
+                  <Typography
+                    sx={{ fontWeight: "bold", fontSize: "24px", mb: "13px" }}
+                  >
+                    عضویت طراح یا عکاس
+                  </Typography>
                 </Box>
                 <Box>
-                  <Button variant="contained" width='119px' height='32px' sx={{backgroundColor:'colors.pink', borderRadius:'10px', boxShadow:'none'}}>عضویت طراح یا عکاس</Button>
+                  <Button
+                    variant="contained"
+                    width="119px"
+                    height="32px"
+                    sx={{
+                      backgroundColor: "colors.pink",
+                      borderRadius: "10px",
+                      boxShadow: "none",
+                      "&:hover": {
+                        backgroundColor: "colors.pink",
+                      },
+                    }}
+                  >
+                    عضویت طراح یا عکاس
+                  </Button>
                 </Box>
               </Box>
             </Grid>
