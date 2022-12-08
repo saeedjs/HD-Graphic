96796f147e66c2b3aa18c73714757b6b73fd1113
saeedjs <saied.2fm83@gmail.com>
2022-12-08 13:23:34 +0330

    saeed

--- components/Header/HeaderCategory.js
+++ components/Header/HeaderCategory.js
@@ -5,54 +5,35 @@ const HeaderCategory = () => {
     <>
       <Box
         sx={{
-          backgroundColor: "",
           width: "100%",
-          height: "30%",
-          display: "flex",
-          justifyContent: "center",
-          p: 5,
+          height: "500px",
+          backgroundImage: "linear-gradient(to  left, white, #EEEEEE)",
+          position: "relative",
         }}
       >
-        <img sx={{ position: "absolute" }} src="./images/dv.png" />
-        <Box
-          sx={{
-            width: "50%",
-            height: "500px",
-            overFlow: "hidden",
-          }}
-        >
-          <Typography
-            sx={{
-              fontSize: {
-                lg: "24px",
-                md: "22px",
-                sm: "20px",
-                xs: "18px",
-              },
-              fontWeight: "bold",
-            }}
-          >
-            بهترین قالب اینستاگرام
-          </Typography>
-          <Typography
-            sx={{
-              fontSize: {
-                lg: "14px",
-                md: "12px",
-                sm: "10px",
-                xs: "8px",
-              },
-              fontWeight: "small",
-            }}
-          >
-            ورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
-            استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در
-            ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
-            کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی
-            در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را
-            می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی
-            الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد{" "}
-          </Typography>
+        <Box sx={{position:'absolute', display:'flex', justifyContent:'space-between', width:'100%', height:'100%'}}>
+          <Box sx={{ mr: "-270px", maxWidth: "100%", display:'flex', alignItems:'center' }}>
+            <img style={{ maxWidth: "100%" }} src="/images/dv.png" />
+          </Box>
+          <Box sx={{width:'50%', display:'flex', flexDirection:'column', justifyContent:'center', height:'100%'}}>
+            <Box sx={{mb:'20px'}}>
+              <Typography sx={{fontSize:'24px', fontWeight:'bold'}}>بهترین قالب های اینستاگرام</Typography>
+            </Box>
+            <Box>
+              <Typography sx={{fontSize:'14px', lineHeight:'2', color:'#393E46'}}>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد.</Typography>
+            </Box>
+          </Box>
+          <Box>
+            <Box>
+              <img style={{maxWidth:'100%'}} src="/images/qqq.png" />
+            </Box>
+            <Box>
+              <img style={{maxWidth:'100%'}} src="/images/eee.png" />
+            </Box>
+            <Box>
+              <img style={{maxWidth:'100%'}} src="/images/dwad.png" />
+            </Box>
+          </Box>
         </Box>
       </Box>
     </>
