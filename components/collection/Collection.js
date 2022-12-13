ea92147db595e36756231004cae502bff8baa9c7
saeedjs <saied.2fm83@gmail.com>
2022-12-13 11:14:25 +0330

    saeed

---
+++ components/collection/Collection.js
@@ -0,0 +1,18 @@
+import { Grid } from "swiper";
+
+const Collection = () => {
+  return (
+    <>
+      <Box>
+        <Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6}></Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default Collection;
---
+++ components/collection/CollectionComp.js
@@ -0,0 +1,14 @@
+const CollectionComp = () => {
+  return (
+    <>
+      <Box>
+        <Box>title</Box>
+        <Grid>
+          <Grid item xs={6}></Grid>
+          <Grid item xs={6}></Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+export default CollectionComp;
