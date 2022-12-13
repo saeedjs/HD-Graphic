a15ef1ca08a4f6bc26e6fc53aa096b154eb116ce
abolfazle79 <abolfazle.abolghasemi79@gmail.com>
2022-12-13 14:39:15 +0330

    asd

--- components/collection/Collection.js
+++ components/collection/Collection.js
@@ -1,11 +1,13 @@
 import { Grid } from "@mui/material";
 import { Container } from "@mui/system";
+import CollectionBanner from "./CollectionBanner";
 import CollectionComp from "./CollectionComp";
 import Collections from "./Collections";
 
 const Collection = () => {
   return (
     <>
+      <CollectionBanner />
       <Container>
         <Collections />
       </Container>
