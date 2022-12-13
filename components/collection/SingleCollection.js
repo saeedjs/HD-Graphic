7f9def19c1e823805ee93b52dd5ae3d0b9f0a3e1
saeedjs <saied.2fm83@gmail.com>
2022-12-13 14:38:09 +0330

    ddffdf

--- components/collection/SingleCollection.js
+++ components/collection/SingleCollection.js
@@ -1,13 +1,20 @@
-import { Box, Grid } from "@mui/material";
+import { Box, Grid, Typography } from "@mui/material";
 import CollectionImg from "./CollectionImg";
 
 const SingleCollection = (props) => {
   return (
     <>
       <Box>
-        <CollectionImg />
+        <CollectionImg data={props.data} />
       </Box>
-      <Box>{props.subc}</Box>
+      <Box sx={{display:'flex', justifyContent:'space-between', mt:'10px'}}>
+        <Box>
+            <Typography sx={{fontSize:'14px', fontWeight:'bold', fontFamily:'iranYekan', color:'colors.gray'}}>{props.data.subc}</Typography>
+        </Box>
+        <Box sx={{width:'66px', height:'28px', backgroundColor:'colors.white', borderRadius:'5px'}}>
+            <Typography sx={{fontSize:'14px' , color:'colors.gray', textAlign:'center', height:'100%'}}>96 فایل</Typography>
+        </Box>
+      </Box>
     </>
   );
 };
