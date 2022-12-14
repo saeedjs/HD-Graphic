41e78ffdce303142386545fa242e2d8132d67460
saeedjs <saied.2fm83@gmail.com>
2022-12-14 13:28:13 +0330

    saeed

--- components/TitleComponents.js
+++ components/TitleComponents.js
@@ -1,10 +1,16 @@
 import { Box, Typography } from "@mui/material";
 import { propsToClassKey } from "@mui/styles";
+import Link from "next/link";
 
 const TitleComponents = (props) => {
   return (
     <>
-      <Box sx={{ display: "flex", justifyContent:{md:'space-between', xs:'space-around'}}}>
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: { md: "space-between", xs: "space-around" },
+        }}
+      >
         <Box>
           <Box sx={{ display: "flex" }}>
             <Box
@@ -16,21 +22,46 @@ const TitleComponents = (props) => {
               }}
             ></Box>
             <Box sx={{ mr: "10px" }}>
-              <Typography sx={{ fontSize: "16px" }}>{props.title}</Typography>
+              <Typography
+                sx={{
+                  color: props.tcolor ? props.tcolor : "colors.dark",
+                  fontSize: "16px",
+                }}
+              >
+                {props.title}
+              </Typography>
             </Box>
             <Box sx={{ mr: "35px" }}>
-              <Typography sx={{ color: "colors.green", fontSize: "14px" }}>
-                مشاهده همه
-              </Typography>
+              <Link href={"#"}>
+                <Typography
+                  sx={{
+                    color: props.allcolor ? props.alltcolor : "colors.green",
+                    fontSize: "14px",
+                  }}
+                >
+                  مشاهده همه
+                </Typography>
+              </Link>
             </Box>
           </Box>
         </Box>
-        <Box sx={{display:{
-          md:"block",
-          xs:'none'
-        }}}>
+        <Box
+          sx={{
+            display: {
+              md: "block",
+              xs: "none",
+            },
+          }}
+        >
           <Box>
-            <Typography sx={{fontSize:'14px', color:'colors.green'}} >{props.mount}</Typography>
+            <Typography
+              sx={{
+                fontSize: "14px",
+                color: props.mcolor ? props.mcolor : "colors.green",
+              }}
+            >
+              {props.mount}
+            </Typography>
           </Box>
         </Box>
       </Box>
--- components/collection/SpecialCollection.js
+++ components/collection/SpecialCollection.js
@@ -1,10 +1,9 @@
 import { Box, Grid } from "@mui/material";
+import TitleComponents from "../TitleComponents";
 import CollectionComp from "./CollectionComp";
 const SpecialCollection = () => {
   const data = [
     {
-      title: "کلکسیون های لایه باز",
-      mount: "بیش از 800 طرح",
       0: {
         limg: "/images/363.png",
         timg: "/images/987.png",
@@ -19,8 +18,6 @@ const SpecialCollection = () => {
       },
     },
     {
-      title: "کلکسیون های تصاویر",
-      mount: "بیش از 5200 طرح",
       0: {
         limg: "/images/666.png",
         timg: "/images/656.png",
@@ -37,12 +34,19 @@ const SpecialCollection = () => {
   ];
   return (
     <>
-      <Box>
+      <Box mt={8}>
         <Grid container sx={{ justifyContent: "center" }}>
-          <Grid item xl={6} lg={7} md={9} sm={12} xs={12} mt={8}>
+          <Grid item xl={11.5} lg={7} md={9} xs={12}>
+            <TitleComponents
+              title={"کلکسیون های ویژه"}
+              mount={"بیش از 4000 طرح"}
+              tcolor={"#F6416C"}
+            />
+          </Grid>
+          <Grid item xl={6} lg={7} md={9} sm={12} xs={12}>
             <CollectionComp data={data[0]} />
           </Grid>
-          <Grid item xl={6} lg={7} md={9} sm={12} xs={12} mt={8}>
+          <Grid item xl={6} lg={7} md={9} sm={12} xs={12}>
             <CollectionComp data={data[1]} />
           </Grid>
         </Grid>
