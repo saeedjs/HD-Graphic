d093a79a4331c8238e13b159bf78a5c79dbd2566
saeedjs <saied.2fm83@gmail.com>
2022-12-19 17:43:59 +0330

    saeed

--- components/dashboard/MainDashboard.js
+++ components/dashboard/MainDashboard.js
@@ -18,7 +18,7 @@ const MAinDashboard = ({children}) => {
           </Grid>
           <Grid item xs={12} sm={9} lg={10}>
             <Box
-              sx={{ borderRight: "2px solid #e9e9e9", height: "100%", p: 2 }}
+              sx={{ borderRight: "2px solid #e9e9e9", minHeight: "1200px", pt:'30px', pr:'45px'}}
             >
               {children}
             </Box>
