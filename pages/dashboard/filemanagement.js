8a923c845e187d238dc617aa6ad9209ce84cd609
abolfazle79 <abolfazle.abolghasemi79@gmail.com>
2022-12-19 13:54:22 +0330

    aasdasdasdasd

--- pages/dashboard/filemanagement.js
+++ pages/dashboard/filemanagement.js
@@ -1,13 +1,17 @@
 import { Box } from "@mui/system";
 import MAinDashboard from "../../components/dashboard/MainDashboard";
+import ProfileFileManager from "../../components/dashboard/ProfileFileManager";
 
 const filemanagement = () => {
-    return(<>
-    <MAinDashboard>
-        <Box>
+  return (
+    <>
+      <MAinDashboard>
+        {/* <Box>
             filemanagement
-        </Box>
-    </MAinDashboard>
-    </>)
-}
+        </Box> */}
+        <ProfileFileManager />
+      </MAinDashboard>
+    </>
+  );
+};
 export default filemanagement;
