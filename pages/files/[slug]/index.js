e48e018ff48d1f0c465ff89cab666fa0a5d50742
saeedjs <saied.2fm83@gmail.com>
2022-12-20 16:11:16 +0330

    saeed

--- pages/files/[slug]/index.js
+++ pages/files/[slug]/index.js
@@ -2,10 +2,6 @@ import { Typography } from "@mui/material";
 import axios from "axios";
 import { useRouter } from "next/router";
 import Filter from "../../../components/Filter";
-<<<<<<< HEAD
-=======
-
->>>>>>> 7b695d9734b3d7b7102099e8c25e91e625d5442c
 const fileSlugPage = ({ category }) => {
   const title = category[0].category.title;
   return (
@@ -21,10 +17,6 @@ const fileSlugPage = ({ category }) => {
       >
         {title}
       </Typography>
-<<<<<<< HEAD
-=======
-
->>>>>>> 7b695d9734b3d7b7102099e8c25e91e625d5442c
       <Filter category={category} />
     </>
   );
