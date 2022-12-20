1658a2ba31669355594ad04b95fa922ee84822e9
abolfazle79 <abolfazle.abolghasemi79@gmail.com>
2022-12-20 16:25:09 +0330

    a

--- pages/files/[slug]/index.js
+++ pages/files/[slug]/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import Filter from "../../../components/Filter";
 
-const fileSlugPage = ({ category }) => {
+const fileSlugPage = ({ category, colors }) => {
   const title = category[0].category.title;
   return (
     <>
@@ -19,20 +19,19 @@ const fileSlugPage = ({ category }) => {
         {title}
       </Typography>
 
-      <Filter category={category} />
+      <Filter category={category} colors={colors} />
     </>
   );
 };
 
 export async function getServerSideProps(params) {
   try {
-    const res = await axios.get(
-      `https://hdgraphic.ir/api/v1/files/category/${params.query.slug}`
-    );
-    console.log(res);
+    const res = await axios.get(`https://hdgraphic.ir/api/v1/files/category/${params.query.slug}`);
+    const resColors = await axios.get(`https://hdgraphic.ir/api/v1/files/colors`);
     return {
       props: {
         category: res.data,
+        colors: resColors.data,
       },
     };
   } catch (error) {
