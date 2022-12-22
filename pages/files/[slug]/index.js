4a3a1a4383ba1f28c90836dc65153679db371d7b
saeedjs <saied.2fm83@gmail.com>
2022-12-22 10:37:11 +0330

    saeed

--- pages/files/[slug]/index.js
+++ pages/files/[slug]/index.js
@@ -4,27 +4,8 @@ import { useRouter } from "next/router";
 import Filter from "../../../components/filter/Filter";
 
 const fileSlugPage = ({ category, colors }) => {
-<<<<<<< HEAD
-  // const title = category[0].category.title;
-=======
-  // const router = useRouter();
->>>>>>> 823285a33b34a185e84ce056174be9ec12daf6bb
   return (
     <>
-      {/* <Typography
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          fontSize: "2rem",
-          fontWeight: "bold",
-          my: 4,
-        }}
-<<<<<<< HEAD
-      >
-      </Typography> */}
-=======
-      ></Typography>
->>>>>>> 823285a33b34a185e84ce056174be9ec12daf6bb
       <Filter category={category} colors={colors} />
     </>
   );
@@ -34,18 +15,13 @@ export async function getServerSideProps(params) {
   let page = 1;
   if (params.query.page) page = params.query.page;
   try {
-<<<<<<< HEAD
-    const res = await axios.get(`https://hdgraphic.ir/api/v1/files/category/${params.query.slug}?page=${page}`);
-    const resColors = await axios.get(`https://hdgraphic.ir/api/v1/files/colors`);
-    console.log(resColors)
-=======
     const res = await axios.get(
       `https://hdgraphic.ir/api/v1/files/category/${params.query.slug}?page=${page}`
     );
     const resColors = await axios.get(
       `https://hdgraphic.ir/api/v1/files/colors`
     );
->>>>>>> 823285a33b34a185e84ce056174be9ec12daf6bb
+    console.log(resColors);
     return {
       props: {
         category: res.data.items,
