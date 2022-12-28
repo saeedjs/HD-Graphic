454055f49a7f2fbabf943b83f52fcf0f8638e116
saeedjs <saied.2fm83@gmail.com>
2022-12-28 11:31:32 +0330

    Merge branch 'master' of https://github.com/saeedjs/HD-Graphic

---
+++ pages/productSingle.js
@@ -0,0 +1,24 @@
+import { propsToClassKey } from "@mui/styles";
+import SingleProduct from "../components/products/SingleProduct";
+
+const productSingle = () => {
+  return (
+    <>
+      <SingleProduct />
+    </>
+  );
+};
+
+export default productSingle;
+
+// export async function  getServerSideProps (){
+
+//   const res = await axios.get("/api/v1/products/:slug")
+//   const data = await data.json()
+
+//   return{
+//     props:{
+//       product: res.data.product,
+//     }
+//   }
+// }
