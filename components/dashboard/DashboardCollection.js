29d4c17e7d8437af6f60f734b76dff8a12e2fadf
saeedjs <saied.2fm83@gmail.com>
2022-12-29 11:00:23 +0330

    saeed

--- components/dashboard/DashboardCollection.js
+++ components/dashboard/DashboardCollection.js
@@ -1,18 +1,21 @@
 import { Grid, Typography } from "@mui/material";
 import { Box } from "@mui/system";
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import DashboardCollectionComp from "./DashboardCollectionComp";
-
 const DashboardCollection = () => {
+  const [access, setAccess] = useState('');
+  useEffect(() => {
+    setAccess(localStorage.getItem('access'));
+  }, [access])
   const [collections, setCollections] = useState([]);
+  // const access = localStorage.getItem('access');
   axios
     .post("https://hdgraphic.ir/api/v1/users/user-collections",{}, {
       headers: {
         "Content-Type": "application/json",
         Authorization:
-          "Bearer " +
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcyMjk4NTMyLCJpYXQiOjE2NzIyOTQ5MzIsImp0aSI6ImEzOWZkZDI2YTAyMTQzMzM4MjA0MThkZjUwNmMxOGM4IiwidXNlcl9pZCI6NTJ9.BxhHQfhZn4M_Ij1TqQzDgQ1OFiH-EW6Z9UmGc8j3BSU",
+          `Bearer ${access}` 
       },
     })
     .then((result) => {
@@ -22,26 +25,6 @@ const DashboardCollection = () => {
       console.log(err);
     });
 
-  const data = [
-    {
-      limg: "/images/vfv.png",
-      timg: "/images/66.png",
-      bimg: "/images/354.png",
-      subc: "موکاپ های خرید",
-    },
-    {
-      limg: "/images/vfv.png",
-      timg: "/images/66.png",
-      bimg: "/images/354.png",
-      subc: "موکاپ های خرید",
-    },
-    {
-      limg: "/images/vfv.png",
-      timg: "/images/66.png",
-      bimg: "/images/354.png",
-      subc: "موکاپ های خرید",
-    },
-  ];
   return (
     <>
       {/* <DashboardCollectionComp data={data} /> */}
