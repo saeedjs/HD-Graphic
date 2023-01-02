36d8f607f4378a895cf8c4867fbd375b37f3fcec
saeedjs <saied.2fm83@gmail.com>
2023-01-02 09:04:51 +0330

    blog

--- components/blog/AllBlog.js
+++ components/blog/AllBlog.js
@@ -1,5 +1,7 @@
 import { Grid } from "@mui/material";
 import { Box, Container } from "@mui/system";
+import { Mypagination } from "../Mypagination";
+import BlogNavBar from "./BlogNavBar";
 import SingleBlog from "./SingleBlog";
 
 const AllBlog = ({ blogs }) => {
@@ -7,13 +9,14 @@ const AllBlog = ({ blogs }) => {
     <>
       <Box>
         <Container>
-          <Grid container>
+          <Grid container sx={{ justifyContent: "center", mb:'30px' }}>
             {blogs.map((blog) => (
-              <Grid item xs={4}>
+              <Grid item lg={4} md={5} sm={7} xs={10}>
                 <SingleBlog blog={blog} />
               </Grid>
             ))}
           </Grid>
+          <Mypagination mount={{page_count: 3}} />
         </Container>
       </Box>
     </>
